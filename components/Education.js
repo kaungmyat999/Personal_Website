6d90a72b7985f64bd1d5f07bbdfa1a509602de07
kaungmyat <kaungmyat@kaungmyat.mynetworksettings.com>
2024-12-18 18:47:50 -0500

    Modified Color and One Porject Name

--- components/Education.js
+++ components/Education.js
@@ -9,9 +9,9 @@ export default function Education() {
             <div className='edu'>
                 <Image src='/images/cu_boulder_logo.png' alt='cuboulder_logo'  width={70} height={70} className='edu_logo'/>
                 <div className='edu_text'>
-                    <p className='edu_name'>Master Of Computer Science<br/>
+                    <p className='edu_name'>Master of Science in Computer Science<br/>
                     <span className='edu_institute'>University Of Colorado Boulder</span>
-                    <br/><span >(Current GPA: 4.0)</span>
+                    <br/><span className='gpa'>(Current GPA: 4.0)</span>
                     </p>
                 </div>
             </div>
