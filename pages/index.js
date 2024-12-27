4a3f39b8c758d3e31dfd7367f5c9989f096ad850
kaungmyat <kaungmyat@kaungmyat.mynetworksettings.com>
2024-12-27 02:02:47 -0500

    Added Animation

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ import path from 'path';
 import Certification from "../components/Certification";
 import Projects from "../components/Projects";
 import Education from '../components/Education'
-
+import { motion } from "motion/react"
 export default function Blog({certs,pjs}) {
     
     return (<div className="main">
@@ -23,34 +23,128 @@ export default function Blog({certs,pjs}) {
                   wrapper="h1"
                 />
               </div>
-              <p>I’m a US-based machine learning and software development engineer who fueled by curiosity and a commitment to continuous learning, which has equipped me with a solid foundation in software engineering and machine learning. I’m eager to apply my skills in real-world projects, and I’m driven by the desire to innovate and solve complex problems.</p>
-              <div className="contact">
-                <div className="contact-left"><span>Contacts:</span></div>
-                <div className="contact-right">
-                  <Link href='https://github.com/kaungmyat999?tab=repositories' aria-label='checkout github' ><Image src='/images/github_logo.png' width={40} height={40} alt='github_logo'/></Link>
-                  <Link href='https://www.linkedin.com/in/george-kyaw-472498298/' aria-label='checkout linkedin' ><Image src='/images/linkedin_logo.png' width={40} height={40} alt='linkedin_logo'/></Link>
-                  <Link href='https://x.com/KaungMy51486739' aria-label='checkout x social media'><Image src='/images/TwitterX.png'width={40} height={40} alt='twitter_logo'/></Link>
-                  <Link href='mailto:kaky2698@colorado.edu' aria-label='checkout email'><Image src='/images/mail_icon.png'width={40} height={40} alt='email_logo' /></Link>
+              <motion.div
+                initial={{ y : -60, opacity: 0, scale: 1}}
+                animate={{ y: 0,opacity: 10, scale: 1}}
+                transition={{
+                  duration: 0.8,
+                  delay: 0.7,
+                  ease: "linear"
+                }}
+              >
+                <p>I’m a US-based machine learning and software development engineer who fueled by curiosity and a commitment to continuous learning, which has equipped me with a solid foundation in software engineering and machine learning. I’m eager to apply my skills in real-world projects, and I’m driven by the desire to innovate and solve complex problems.</p>
+                <div className="contact">
+                  <div className="contact-left"><span>Contacts:</span></div>
+                  <div className="contact-right">
+                    <Link href='https://github.com/kaungmyat999?tab=repositories' aria-label='checkout github' ><Image src='/images/github_logo.png' width={40} height={40} alt='github_logo'/></Link>
+                    <Link href='https://www.linkedin.com/in/george-kyaw-472498298/' aria-label='checkout linkedin' ><Image src='/images/linkedin_logo.png' width={40} height={40} alt='linkedin_logo'/></Link>
+                    <Link href='https://x.com/KaungMy51486739' aria-label='checkout x social media'><Image src='/images/TwitterX.png'width={40} height={40} alt='twitter_logo'/></Link>
+                    <Link href='mailto:kaky2698@colorado.edu' aria-label='checkout email'><Image src='/images/mail_icon.png'width={40} height={40} alt='email_logo' /></Link>
+                  </div>
                 </div>
-              </div>
+
+              </motion.div>
+
 
               
             </div>
             <div className="right-side">
-              <Image className="profile" src='/images/IMG_1505.JPG' alt='Profile_Pic' width={350} height={450}/>
-              
+              <motion.div
+                initial={{ x : 400, opacity: 0 , scale: 1 }}
+                animate={{x : [600, -80, 0],opacity: [0,0.25,0.5,1], scale: 1}}
+                
+                transition={{
+                  duration: 0.8,
+                  delay: 0.9,
+                  ease: "easeIn"
+                }}
+              >
+                <Image className="profile" src='/images/IMG_1505.JPG' alt='Profile_Pic' width={350} height={450}/>
+              </motion.div>
             </div>
             
           </div>
-          <div className="sections">
-            <Education />
-          </div>
-          <div className="sections">
-            <Projects pjs={pjs}/>
-          </div>
-          <div className="sections">
-            <Certification certs={certs}/>
-          </div>
+          <motion.div
+                initial={{ y : -100, opacity: 0, scale: 1}}
+                
+                whileInView={{
+                  y: 0,opacity: 10, scale: 1
+                }}
+                
+                transition={{
+                  duration: 0.8,
+                  delay: 0.6,
+                  ease: "linear",
+                  staggerChildren: 0.3
+                }}
+                
+               
+                viewport={{ once: true }}
+              >
+            <div className="sections">
+              <Education />
+            </div>
+          </motion.div>
+          <motion.div
+                initial={{ y : -60, opacity: 0, scale: 1}}
+                
+                whileInView={{
+                  y: [200,100,0],opacity: 10, scale: 1
+                }}
+                
+                transition={{
+                  duration: 0.8,
+                  delay: 0.6,
+                  ease: "linear",
+                  
+                }}
+                
+               
+                viewport={{ once: true }}
+              >
+          </motion.div>
+          <motion.div
+                initial={{ y : -60, opacity: 0, scale: 1}}
+                
+                whileInView={{
+                  y: 0,opacity: 10, scale: 1
+                }}
+                
+                transition={{
+                  duration: 0.8,
+                  delay: 0.6,
+                  ease: "linear",
+                  staggerChildren: 0.3
+                }}
+                
+               
+                viewport={{ once: true }}
+              >
+            <div className="sections">
+              <Projects pjs={pjs}/>
+            </div>
+          </motion.div>
+          <motion.div
+                initial={{ y : -60, opacity: 0, scale: 1}}
+                
+                whileInView={{
+                  y: 0,opacity: 10, scale: 1
+                }}
+                
+                transition={{
+                  duration: 0.8,
+                  delay: 0.6,
+                  ease: "linear",
+                  staggerChildren: 0.3
+                }}
+                
+               
+                viewport={{ once: true }}
+              >
+            <div className="sections">
+              <Certification certs={certs}/>
+            </div>
+          </motion.div>
           
         </div>
         <Footer />
